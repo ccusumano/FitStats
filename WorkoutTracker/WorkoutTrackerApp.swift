63bd02d8db8bba417fc4cc6bef6dd854e959e701
carl <carlcusumano3@gmail.com>
2025-12-30 11:14:51 -0800

    Initial Commit

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,21 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Carl on 12/30/25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct WorkoutTrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
